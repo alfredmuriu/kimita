ffe4bfb282552fef19d5dd871e3d3cacd4f1f9ca
alfredmuriu <alfredmuriu19@gmail.com>
2026-03-15 22:18:08 +0300

    feat: add Google Analytics 4 tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="js ss-loaded">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-H7FKY9RHLN"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-H7FKY9RHLN');
+          `}
+        </Script>
+      </head>
       <body id="top" className="ss-show">
         {children}
         <Analytics />
